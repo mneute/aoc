e0dc2f11d00fa900c6ee0175829101dd249f8172
mneute <morgan.neute@gmail.com>
2024-12-27 14:28:54 +0100

    Day 2

---
+++ day2/day2.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+$startTime = microtime(true);
+memory_reset_peak_usage();
+
+$pt1 = $pt2 = 0;
+
+foreach (file(__DIR__.'/input.txt', FILE_IGNORE_NEW_LINES) as $line) {
+    $data = explode(' ', $line);
+
+    if (isSafe($data)) {
+        $pt1++;
+        $pt2++;
+
+        continue;
+    }
+
+    foreach (range(0, count($data) - 1) as $i) {
+        $copy = $data;
+        array_splice($copy, $i, 1);
+
+        if (isSafe($copy)) {
+            $pt2++;
+
+            continue 2;
+        }
+    }
+}
+
+printf('Part 1 : %d'.PHP_EOL, $pt1);
+printf('Part 2 : %d'.PHP_EOL, $pt2);
+
+printf('Execution time : %s seconds'.PHP_EOL, round(microtime(true) - $startTime, 4));
+printf('  Memory usage : %s Mib'.str_repeat(PHP_EOL, 2), round(memory_get_peak_usage() / (2 ** 20), 4));
+
+function isSafe(array $data): bool
+{
+    $max = count($data) - 2;
+    $steps = [];
+    for ($i = 0; $i <= $max; $i++) {
+        $steps[] = $data[$i + 1] - $data[$i];
+    }
+
+    return array_all($steps, static fn (int $step): bool => 1 <= $step && $step <= 3)
+        || array_all($steps, static fn (int $step): bool => -3 <= $step && $step <= -1);
+}
