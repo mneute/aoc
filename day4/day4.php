6da2136dc096cffa156cdebb5e4172c7a32c5f2b
mneute <morgan.neute@gmail.com>
2024-12-31 12:23:00 +0100

    Day 4

--- day4/day4.php
+++ day4/day4.php
@@ -2,76 +2,72 @@
 
 declare(strict_types=1);
 
-const WORD = 'XMAS';
-$lettersToLookFor = strlen(WORD) - 1;
+$startTime = microtime(true);
+memory_reset_peak_usage();
 
-$letters = [];
+$input = array_map(str_split(...), file(__DIR__.'/input.txt', FILE_IGNORE_NEW_LINES));
 
-function parseFile(array &$letters): void
-{
-    $file = @fopen(__DIR__.'/input.txt', 'rb');
-    if (!$file) {
-        throw new \RuntimeException('Could not open the file');
-    }
+$pt1 = $pt2 = 0;
 
-    while (($line = fgets($file)) !== false) {
-        $line = trim($line);
-
-        $letters[] = str_split($line);
+for ($i = 0; $i < count($input); $i++) {
+    for ($j = 0; $j < count($input[$i]); $j++) {
+        if ('X' === $input[$i][$j]) {
+            $pt1 += tryPart1($i, $j);
+        }
     }
 }
 
-function tryPosition(int $i, int $j): int
+printf('Part 1 : %d'.PHP_EOL, $pt1);
+printf('Part 2 : %d'.PHP_EOL, $pt2);
+
+printf('Execution time : %s seconds'.PHP_EOL, round(microtime(true) - $startTime, 4));
+printf('  Memory usage : %s Mib'.str_repeat(PHP_EOL, 2), round(memory_get_peak_usage() / (2 ** 20), 4));
+
+function tryPart1(int $i, int $j): int
 {
-    global $letters, $lettersToLookFor, $maxI, $maxJ;
+    global $input;
+
+    $maxI = count($input);
+    $maxJ = count($input[0]);
+    $steps = 3;
+
+    $directions = [
+        [0, 1], // ➡
+        [1, 1], // ↘
+        [1, 0], // ⬇
+        [1, -1], // ↙
+        [0, -1], // ⬅
+        [-1, -1], // ↖
+        [-1, 0], // ⬆
+        [-1, 1], // ↗
+    ];
 
     $count = 0;
 
-    // ➡
-    if (($j + $lettersToLookFor) < $maxJ && tryDirection($i, 0, $j, 1)) {
-        $count++;
-    }
-    // ↘
-    if (($j + $lettersToLookFor) < $maxJ && ($i + $lettersToLookFor) < $maxI && tryDirection($i, 1, $j, 1)) {
-        $count++;
-    }
-    // ⬇
-    if (($i + $lettersToLookFor) < $maxI && tryDirection($i, 1, $j, 0)) {
-        $count++;
-    }
-    // ↙
-    if (($j - $lettersToLookFor) >= 0 && ($i + $lettersToLookFor) < $maxI && tryDirection($i, 1, $j, -1)) {
-        $count++;
-    }
-    // ⬅
-    if (($j - $lettersToLookFor) >= 0 && tryDirection($i, 0, $j, -1)) {
-        $count++;
-    }
-    // ↖
-    if (($j - $lettersToLookFor) >= 0 && ($i - $lettersToLookFor) >= 0 && tryDirection($i, -1, $j, -1)) {
-        $count++;
-    }
-    // ⬆
-    if (($i - $lettersToLookFor) >= 0 && tryDirection($i, -1, $j, 0)) {
-        $count++;
-    }
-    // ↗
-    if (($j + $lettersToLookFor) < $maxJ && ($i - $lettersToLookFor) >= 0 && tryDirection($i, -1, $j, 1)) {
-        $count++;
+    foreach ($directions as $direction) {
+        // bounds check
+        $end = [
+            $i + ($direction[0] * $steps),
+            $j + ($direction[1] * $steps),
+        ];
+
+        if ($end[0] < 0 || $end[0] >= $maxI || $end[1] < 0 || $end[1] >= $maxJ) continue;
+
+        if (tryDirectionPart1($i, $direction[0], $j, $direction[1])) $count++;
     }
 
     return $count;
 }
 
-function tryDirection(int $i, int $stepI, int $j, int $stepJ): bool
+function tryDirectionPart1(int $i, int $stepI, int $j, int $stepJ): bool
 {
-    global $letters, $lettersToLookFor;
+    global $input;
 
     $i += $stepI;
     $j += $stepJ;
 
-    foreach (range(1, $lettersToLookFor) as $index) {
-        if (WORD[$index] !== $letters[$i][$j]) {
+    foreach (['M', 'A', 'S'] as $letter) {
+        if ($letter !== $input[$i][$j]) {
             return false;
         }
 
@@ -81,20 +77,3 @@ function tryDirection(int $i, int $stepI, int $j, int $stepJ): bool
 
     return true;
 }
-
-parseFile($letters);
-
-$maxI = count($letters);
-$maxJ = count($letters[0]);
-
-$count = 0;
-
-for ($i = 0; $i < count($letters); $i++) {
-    for ($j = 0; $j < count($letters[$i]); $j++) {
-        if (WORD[0] === $letters[$i][$j]) {
-            $count += tryPosition($i, $j);
-        }
-    }
-}
-
-printf('X : %d'.PHP_EOL, $count);
