17f9e2998d5e83ddae03e63c46885f83367a7907
mneute <morgan.neute@gmail.com>
2025-08-23 22:27:47 +0200

    Add 03/12/2023

---
+++ src/Year2023/Day03/Day03.php
@@ -0,0 +1,143 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Year2023\Day03;
+
+use App\AbstractPuzzle;
+use App\Result;
+
+final class Day03 extends AbstractPuzzle
+{
+    private const string INPUT_FILE = __DIR__ . '/input.txt';
+
+    // @formatter:off
+    private const array DIRECTIONS = [
+        'North'      => [-1, 0],
+        'North-East' => [-1, 1],
+        'East'       => [0, 1],
+        'South-East' => [1, 1],
+        'South'      => [1, 0],
+        'South-West' => [1, -1],
+        'West'       => [0, -1],
+        'North-West' => [-1, -1],
+    ];
+    // @formatter:on
+
+    /**
+     * @var list<list<string>>
+     */
+    private array $map = [];
+
+    public function run(): Result
+    {
+        $this->parseFile();
+
+        $pt1 = $pt2 = 0;
+
+        // Part 1
+        foreach ($this->map as $i => $line) {
+            $number = new Number();
+
+            foreach ($line as $j => $char) {
+                if (is_numeric($char)) {
+                    $number->addChar($char);
+
+                    if ($number->valid) continue;
+
+                    $number->valid = $this->hasSymbolAround([$i, $j]);
+                } elseif ($number->number !== '') {
+                    if ($number->valid) $pt1 += (int) $number->number;
+
+                    $number->reset();
+                }
+            }
+
+            if ($number->valid) $pt1 += (int) $number->number;
+        }
+
+        // Part 2
+        foreach ($this->map as $i => $line) {
+            foreach ($line as $j => $char) {
+                if ('*' !== $char) continue;
+
+                $numbers = $this->getNumbersAround([$i, $j]);
+
+                if (2 !== count($numbers)) continue;
+
+                $pt2 += array_reduce(
+                    $numbers,
+                    fn (int $carry, int $number): int => $carry * $number,
+                    1
+                );
+            }
+        }
+
+        return new Result($pt1, $pt2);
+    }
+
+    private function parseFile(): void
+    {
+        foreach ($this->readFile(self::INPUT_FILE) as $line) {
+            $this->map[] = str_split($line);
+        }
+    }
+
+    /**
+     * @param array{0: int, 1: int} $coordinates
+     */
+    private function hasSymbolAround(array $coordinates): bool
+    {
+        foreach (self::DIRECTIONS as $direction) {
+            $i = $coordinates[0] + $direction[0];
+            $j = $coordinates[1] + $direction[1];
+
+            if (!isset($this->map[$i][$j])) continue;     // Out of bounds
+            if (is_numeric($this->map[$i][$j])) continue; // Is another number
+            if ('.' !== $this->map[$i][$j]) return true;  // Something else than a point ? Victory
+        }
+
+        return false;
+    }
+
+    /**
+     * @param array{0: int, 1: int} $coordinates
+     * @return list<int>
+     */
+    private function getNumbersAround(array $coordinates): array
+    {
+        /** @var array<int, array<int, true>> $alreadyVisited */
+        $alreadyVisited = [];
+        $numbers = [];
+
+        foreach (self::DIRECTIONS as $direction) {
+            $i = $coordinates[0] + $direction[0];
+            $j = $coordinates[1] + $direction[1];
+
+            if (!isset($this->map[$i][$j])) continue;       // Out of bounds
+            if (!is_numeric($this->map[$i][$j])) continue;  // NaN
+            if ($alreadyVisited[$i][$j] ?? false) continue; // We visited this position when parsing a number
+
+            $alreadyVisited[$i][$j] = true;
+            $number = $this->map[$i][$j];
+
+            $j2 = $j + 1;
+            while (is_numeric($this->map[$i][$j2] ?? '.')) {
+                $number .= $this->map[$i][$j2];
+                $alreadyVisited[$i][$j2] = true;
+                $j2++;
+            }
+
+            $j2 = $j - 1;
+            while (is_numeric($this->map[$i][$j2] ?? '.')) {
+                $number = $this->map[$i][$j2] . $number;
+                $alreadyVisited[$i][$j2] = true;
+                $j2--;
+            }
+
+            $numbers[] = (int) $number;
+        }
+
+        return $numbers;
+    }
+}
---
+++ src/Year2023/Day03/Number.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Year2023\Day03;
+
+final class Number
+{
+    private(set) string $number = '' {
+        get => $this->number;
+    }
+
+    public bool $valid = false {
+        get => $this->valid;
+        set => $this->valid = $value;
+    }
+
+    public function addChar(string $char): void
+    {
+        $this->number .= $char;
+    }
+
+    public function reset(): void
+    {
+        $this->number = '';
+        $this->valid = false;
+    }
+}
