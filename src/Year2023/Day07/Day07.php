efe04796db5c346d973f520d3a4abc75c39e0824
mneute <morgan.neute@gmail.com>
2025-08-26 23:25:50 +0200

    Add 07/12/2023

---
+++ src/Year2023/Day07/Day07.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Year2023\Day07;
+
+use App\AbstractPuzzle;
+use App\Result;
+
+final class Day07 extends AbstractPuzzle
+{
+    private const string INPUT_FILE = __DIR__ . '/input.txt';
+
+    public function run(): Result
+    {
+        $pt1 = $pt2 = 0;
+
+        $hands = [];
+        foreach ($this->readFile(self::INPUT_FILE) as $line) {
+            preg_match('#^(?<hand>[AKQJT2-9]{5})\s+(?<bid>\d+)$#', $line, $matches);
+
+            $hands[] = new Hand($matches['hand'], (int) $matches['bid']);
+        }
+
+        usort($hands, $this->part1Sort(...));
+        foreach ($hands as $index => $hand) {
+            $pt1 += ($index + 1) * $hand->bid;
+        }
+
+        usort($hands, $this->part2Sort(...));
+        foreach ($hands as $index => $hand) {
+            $pt2 += ($index + 1) * $hand->bid;
+        }
+
+        return new Result($pt1, $pt2);
+    }
+
+    private function part1Sort(Hand $a, Hand $b): int
+    {
+        if ($a->part1Type !== $b->part1Type) {
+            return $a->part1Type->value <=> $b->part1Type->value;
+        }
+
+        return $a->part1Weight <=> $b->part1Weight;
+    }
+
+    private function part2Sort(Hand $a, Hand $b): int
+    {
+        if ($a->part2Type !== $b->part2Type) {
+            return $a->part2Type->value <=> $b->part2Type->value;
+        }
+
+        return $a->part2Weight <=> $b->part2Weight;
+    }
+}
---
+++ src/Year2023/Day07/Hand.php
@@ -0,0 +1,125 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Year2023\Day07;
+
+final class Hand
+{
+    private const array CARDS_POINTS_PART_1 = [
+        'A' => 'E',
+        'K' => 'D',
+        'Q' => 'C',
+        'J' => 'B',
+        'T' => 'A',
+    ];
+    private const array CARDS_POINTS_PART_2 = [
+        'A' => 'E',
+        'K' => 'D',
+        'Q' => 'C',
+        'J' => '1', // 👈
+        'T' => 'A',
+    ];
+    private const int ORD_J = 74;
+
+    private(set) Type $part1Type;
+    private(set) int $part1Weight;
+    private(set) Type $part2Type;
+    private(set) int $part2Weight;
+
+    /** @var array<int, int> */
+    private array $charsCount;
+
+    public function __construct(
+        private(set) readonly string $input,
+        private(set) readonly int $bid,
+    ) {
+        $this->part1Type = $this->getPart1Type();
+        $this->part1Weight = $this->getWeight(self::CARDS_POINTS_PART_1);
+
+        $this->part2Type = $this->getPart2Type();
+        $this->part2Weight = $this->getWeight(self::CARDS_POINTS_PART_2);
+    }
+
+    /**
+     * @param array<string, string> $cardsPoints
+     */
+    private function getWeight(array $cardsPoints): int
+    {
+        return intval(
+            str_replace(
+                array_keys($cardsPoints),
+                array_values($cardsPoints),
+                $this->input,
+            ),
+            16
+        );
+    }
+
+    private function getPart1Type(): Type
+    {
+        $this->charsCount = count_chars($this->input, 1);
+
+        if (in_array(5, $this->charsCount, true)) {
+            return Type::FIVE_OF_A_KIND;
+        }
+        if (in_array(4, $this->charsCount, true)) {
+            return Type::FOUR_OF_A_KIND;
+        }
+        if (in_array(3, $this->charsCount, true)) {
+            return in_array(2, $this->charsCount, true)
+                ? Type::FULL_HOUSE
+                : Type::THREE_OF_A_KIND;
+        }
+        if (in_array(2, $this->charsCount, true)) {
+            return 3 === count($this->charsCount)
+                ? Type::TWO_PAIR
+                : Type::ONE_PAIR;
+        }
+
+        return Type::HIGH_CARD;
+    }
+
+    private function getPart2Type(): Type
+    {
+        $jokerCount = $this->charsCount[self::ORD_J] ?? 0;
+
+        if (in_array($jokerCount, [0, 5], true)) {
+            return $this->part1Type;
+        }
+
+        if (1 === $jokerCount) {
+            return match ($this->part1Type) {
+                Type::FOUR_OF_A_KIND => Type::FIVE_OF_A_KIND,
+                Type::THREE_OF_A_KIND => Type::FOUR_OF_A_KIND,
+                Type::TWO_PAIR => Type::FULL_HOUSE,
+                Type::ONE_PAIR => Type::THREE_OF_A_KIND,
+                Type::HIGH_CARD => Type::ONE_PAIR,
+                default => throw new \LogicException('Impossible'),
+            };
+        }
+
+        if (2 === $jokerCount) {
+            return match ($this->part1Type) {
+                Type::FULL_HOUSE => Type::FIVE_OF_A_KIND,
+                Type::TWO_PAIR => Type::FOUR_OF_A_KIND,
+                Type::ONE_PAIR => Type::THREE_OF_A_KIND,
+                default => throw new \LogicException('Impossible'),
+            };
+        }
+
+        if (3 === $jokerCount) {
+            return match ($this->part1Type) {
+                Type::FULL_HOUSE => Type::FIVE_OF_A_KIND,
+                Type::THREE_OF_A_KIND => Type::FOUR_OF_A_KIND,
+                default => throw new \LogicException('Impossible'),
+            };
+        }
+
+        // 4 === $jokerCount
+        return match ($this->part1Type) {
+            Type::FOUR_OF_A_KIND => Type::FIVE_OF_A_KIND,
+            default => throw new \LogicException('Impossible'),
+        };
+    }
+}
