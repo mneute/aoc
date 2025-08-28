4d7cb24e207b253d4d5f8de9cd5ad1c5694d23a8
mneute <morgan.neute@gmail.com>
2025-08-28 14:15:03 +0200

    temp

--- src/Year2023/Day15/Day15.php
+++ src/Year2023/Day15/Day15.php
@@ -9,7 +9,7 @@ use App\Result;
 
 final class Day15 extends AbstractPuzzle
 {
-    private const string INPUT_FILE = __DIR__ . '/input.txt';
+    private const string INPUT_FILE = __DIR__ . '/input-test.txt';
 
     public function run(): Result
     {
@@ -22,18 +22,28 @@ final class Day15 extends AbstractPuzzle
 
         $steps = explode(',', $input);
         foreach ($steps as $step) {
-            $pt1 += array_reduce(
-                str_split($step),
-                fn (int $carry, string $char): int => $this->hash($carry, $char),
-                0
-            );
+            $pt1 += $this->hashInput($step);
+
+            preg_match('#^(?<label>[a-z]+)(?<operation>[=-])(?<focal>\d+)?$#', $step, $matches);
+
+            printf('Label : "%s", operation : "%s"', $matches['label'], $matches['operation']);
+            if (isset($matches['focal'])) {
+                printf(', focal : %d', $matches['focal']);
+            }
+            printf(PHP_EOL);
         }
+
+        printf('%s : %d'.PHP_EOL, 'rn=1', $this->hashInput('rn'));
 
         return new Result($pt1, 0);
     }
 
-    private function hash(int $currentValue, string $char): int
+    private function hashInput(string $input): int
     {
-        return ($currentValue + ord($char)) * 17 % 256;
+        return array_reduce(
+            str_split($input),
+            static fn (int $carry, string $char): int => ($carry + ord($char)) * 17 % 256,
+            0
+        );
     }
 }
