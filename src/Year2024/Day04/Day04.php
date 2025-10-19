28fc89edec5bdbbaf57b357d2a8a86045cec6fa3
mneute <morgan.neute@gmail.com>
2025-10-19 18:20:28 +0200

    Rework 04/12/2024

--- src/Year2024/Day04/Day04.php
+++ src/Year2024/Day04/Day04.php
@@ -9,7 +9,19 @@ use App\Result;
 
 final class Day04 extends AbstractPuzzle
 {
+    private const array DIRECTIONS = [
+        'N' => [-1, 0],
+        'NE' => [-1, 1],
+        'E' => [0, 1],
+        'SE' => [1, 1],
+        'S' => [1, 0],
+        'SW' => [1, -1],
+        'W' => [0, -1],
+        'NW' => [-1, -1],
+    ];
     private static array $input;
+    private static int $maxI;
+    private static int $maxJ;
 
     public function run(): Result
     {
@@ -18,10 +30,12 @@ final class Day04 extends AbstractPuzzle
         foreach ($this->readFile() as $line) {
             self::$input[] = str_split($line);
         }
+        self::$maxI = count(self::$input);
+        self::$maxJ = count(self::$input[0]);
 
-        for ($i = 0; $i < count(self::$input); $i++) {
-            for ($j = 0; $j < count(self::$input[$i]); $j++) {
-                if ('X' === self::$input[$i][$j]) {
+        foreach (self::$input as $i => $line) {
+            foreach ($line as $j => $char) {
+                if ('X' === $char) {
                     $pt1 += $this->tryPart1($i, $j);
                 }
             }
@@ -32,48 +46,37 @@ final class Day04 extends AbstractPuzzle
 
     private function tryPart1(int $i, int $j): int
     {
-        $maxI = count(self::$input);
-        $maxJ = count(self::$input[0]);
         $steps = 3;
-
-        $directions = [
-            '➡' => [0, 1],
-            '↘' => [1, 1],
-            '⬇' => [1, 0],
-            '↙' => [1, -1],
-            '⬅' => [0, -1],
-            '↖' => [-1, -1],
-            '⬆' => [-1, 0],
-            '↗' => [-1, 1],
-        ];
-
         $count = 0;
 
-        foreach ($directions as $direction) {
+        foreach (self::DIRECTIONS as $direction) {
             // bounds check
             $end = [
                 $i + ($direction[0] * $steps),
                 $j + ($direction[1] * $steps),
             ];
 
-            if ($end[0] < 0 || $end[0] >= $maxI || $end[1] < 0 || $end[1] >= $maxJ) continue;
+            if ($end[0] < 0 || $end[0] >= self::$maxI || $end[1] < 0 || $end[1] >= self::$maxJ) continue;
 
-            if ($this->tryDirectionPart1($i, $direction[0], $j, $direction[1])) $count++;
+            if ($this->tryDirectionPart1($i, $j, $direction)) $count++;
         }
 
         return $count;
     }
 
-    private function tryDirectionPart1(int $i, int $stepI, int $j, int $stepJ): bool
+    /**
+     * @param array{0: int, 1: int} $steps
+     */
+    private function tryDirectionPart1(int $i, int $j, array $steps): bool
     {
-        $i += $stepI;
-        $j += $stepJ;
+        $i += $steps[0];
+        $j += $steps[1];
 
         foreach (['M', 'A', 'S'] as $letter) {
             if ($letter !== self::$input[$i][$j]) return false;
 
-            $i += $stepI;
-            $j += $stepJ;
+            $i += $steps[0];
+            $j += $steps[1];
         }
 
         return true;
