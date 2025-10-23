f68ebfc3731ed22bf09757b66cca306c892305b5
mneute <morgan.neute@gmail.com>
2025-10-23 14:45:57 +0200

    Add CommandBuilder

--- .castor/castor.php
+++ .castor/castor.php
@@ -3,20 +3,22 @@
 use Castor\Attribute\AsContext;
 use Castor\Attribute\AsTask;
 use Castor\Context;
+use CommandBuilder\DockerCompose;
+use function Castor\import;
 use function Castor\run;
 
-const DOCKER_COMPOSE = 'docker compose -f docker-compose.yml ';
+import(__DIR__);
 
 #[AsTask(description: 'Build the docker image')]
 function build(): void
 {
-    run(DOCKER_COMPOSE . 'build --pull php');
+    run((new DockerCompose())->build());
 }
 
 #[AsTask(description: 'Starts a bash session inside the container', default: true)]
 function bash(): void
 {
-    run(DOCKER_COMPOSE . 'run --rm php /bin/bash');
+    run((new DockerCompose())->run());
 }
 
 #[AsContext(default: true)]
